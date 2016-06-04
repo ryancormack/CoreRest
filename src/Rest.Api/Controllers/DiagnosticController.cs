8d7287687ac842202ab3394c09bd2045c0fe6c12
Ryan Cormack <ryancormack89@gmail.com>
2016-06-04 15:27:18 +0100

    Added a diagnostic api to get some pointless info

---
+++ src/Rest.Api/Controllers/DiagnosticController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Rest.Models;
+
+namespace Rest.Controllers
+{
+    [Route("api/diagnostic")]
+    public class DiagnosticController : Controller
+    {
+        public IActionResult Info() {
+            var ip = HttpContext.Request.HttpContext.Connection.RemoteIpAddress.ToString();
+            return Json(new Diagnostics(ip));
+        }
+    }
+}
---
+++ src/Rest.Api/Models/Diagnostics.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rest.Models
+{
+    public class Diagnostics
+    {
+        public Diagnostics(string ip) {
+            this.RequestIpAddress = ip;
+        }
+
+        public string RequestIpAddress { get; }
+    }
+}
