684aacf6ceffa3943d6898eedba885c6cce45023
Ryan Cormack <ryancormack89@gmail.com>
2016-08-22 12:15:41 +0100

    Use Google's service worker example. Use a shared layout for all SW pages

--- src/Rest.Api/Features/Offline/OfflineController.cs
+++ src/Rest.Api/Features/Offline/OfflineController.cs
@@ -2,7 +2,7 @@
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
-namespace Rest.Controllers
+namespace Rest.Api.Features.Offline
 {
     public class OfflineController : Controller
     {
